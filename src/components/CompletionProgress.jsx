ed570ed3cb798cf3749e98a6fdc093587b5b14c7
hemasudha <hemasudha@gmail.com>
2025-08-24 18:23:56 +0530

    leaderboard update

--- src/components/CompletionProgress.jsx
+++ src/components/CompletionProgress.jsx
@@ -21,7 +21,7 @@ function CompletionProgress() {
             <div className="w-full bg-gray-200 rounded-full h-2">
               <div
                 className="bg-blue-500 h-2 rounded-full"
-                style={{ width: `${item.progress}%` }}
+                style={{ width: `${item.progress}` }}
               ></div>
             </div>
           </div>
--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 const leaderboardData = [
-  { id: 1, name: "Hema", score: 98, img: "https://i.pravatar.cc/40?img=1", color: "bg-green-100 border-l-4 border-green-500" },
-  { id: 2, name: "Abdul", score: 92, img: "https://i.pravatar.cc/40?img=2", color: "bg-pink-100 border-l-4 border-pink-500" },
-  { id: 3, name: "Lahari", score: 88, img: "https://i.pravatar.cc/40?img=3", color: "bg-blue-100 border-l-4 border-blue-500" },
-  { id: 4, name: "Shankar", score: 82, img: "https://i.pravatar.cc/40?img=4", color: "bg-yellow-100 border-l-4 border-yellow-500" },
-  { id: 5, name: "Nisha", score: 75, img: "https://i.pravatar.cc/40?img=5", color: "bg-purple-100 border-l-4 border-purple-500" },
-  { id: 6, name: "Anand", score: 70, img: "https://i.pravatar.cc/40?img=6", color: "bg-red-100 border-l-4 border-red-500" },
+  { id: 1, name: "Nisha Ankur", score: 98, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT44Osut7lvh-JcO3267T48aPfYQ2MMJWBMCQ&s", color: "bg-green-100 border-l-4 border-green-500" },
+  { id: 2, name: "Shyam Nithin", score: 92, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdQLwDqDwd2JfzifvfBTFT8I7iKFFevcedYg&s", color: "bg-pink-100 border-l-4 border-pink-500" },
+  { id: 3, name: "Dinesh Kumar", score: 88, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdQLwDqDwd2JfzifvfBTFT8I7iKFFevcedYg&s", color: "bg-blue-100 border-l-4 border-blue-500" },
+  { id: 4, name: "Radha Shankar", score: 82, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCIyTZVXyb90oYHRiiX6YkNUc0CnzGwWjI3Q&s", color: "bg-yellow-100 border-l-4 border-yellow-500" },
+  { id: 5, name: "Naveen Kumar", score: 75, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrKxfjTf49GAtu0PpFXK7mKBgqyJ5MfJCgQw&s", color: "bg-purple-100 border-l-4 border-purple-500" },
+  { id: 6, name: "Anand vardhan", score: 70, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNm6BkgT87_Fus1KSC2Bti9okSzIM0kOd5Pw&s", color: "bg-red-100 border-l-4 border-red-500" },
 ];
 
 function Leaderboard() {
@@ -24,12 +24,12 @@ function Leaderboard() {
               <img
                 src={item.img}
                 alt={item.name}
-                className="w-8 h-8 rounded-full border border-gray-300"
+                className="w-9 h-8 rounded-full border border-gray-300"
               />
               <span className="text-sm font-medium">{item.name}</span>
             </div>
 
-            <span className="text-xs font-semibold text-gray-700">
+            <span className="text-md font-semibold text-gray-700">
               Score: {item.score}
             </span>
           </div>
